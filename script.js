d603316602ec8719b86619e18f62445df9558e99
imajefkgvq <imangalikazybek1975@gmail.com>
2024-12-11 15:48:18 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,109 @@
+const img1 = document.getElementById("img1");
+const img2 = document.getElementById("img2");
+const img3 = document.getElementById("img3");
+const img4 = document.getElementById("img4");
+const img5 = document.getElementById("img5");
+const img6 = document.getElementById("img6");
+const img7 = document.getElementById("img7");
+const img8 = document.getElementById("img8");
+const img9 = document.getElementById("img9");
+
+img1.addEventListener("mouseover", () => {
+  img1.src = "cropped_1_1.jpg";
+  img2.src = "cropped_1_2.jpg";
+  img3.src = "cropped_1_2.jpg ";
+  img4.src = "cropped_image_8.jpg";
+  img5.src = " cropped_image_9.jpg";
+  img6.src = " cropped_image_9.jpg";
+  img7.src = " cropped_image_8.jpg";
+  img8.src = "cropped_image_9.jpg ";
+  img9.src = "cropped_image_9.jpg ";
+});
+img2.addEventListener("mouseover", () => {
+  img2.src = "cropped_1_1.jpg";
+  img3.src = "cropped_1_2.jpg";
+  img1.src = "cropped_1_0.jpg";
+  img5.src = "cropped_image_8.jpg";
+  img7.src = "cropped_image_7.jpg";
+  img9.src = "cropped_image_9.jpg";
+  img4.src = "cropped_image_7.jpg";
+  img6.src = "cropped_image_9.jpg";
+  img8.src = "cropped_image_8.jpg";
+});
+img3.addEventListener("mouseover", () => {
+  img1.src = "cropped_1_0.jpg";
+  img2.src = "cropped_1_0.jpg";
+  img3.src = "cropped_1_1.jpg";
+  img4.src = "cropped_image_7.jpg";
+  img5.src = "cropped_image_7.jpg";
+  img6.src = "cropped_image_8.jpg";
+  img7.src = "cropped_image_7.jpg";
+  img8.src = "cropped_image_7.jpg";
+  img9.src = "cropped_image_8.jpg";
+});
+img4.addEventListener("mouseover", () => {
+  img1.src = "cropped_0_1.jpg";
+  img2.src = "cropped_0_2.jpg";
+  img3.src = "cropped_0_2.jpg";
+  img4.src = "cropped_1_1.jpg";
+  img5.src = "cropped_1_2.jpg";
+  img6.src = "cropped_1_2.jpg";
+  img7.src = "cropped_image_8.jpg";
+  img8.src = "cropped_image_9.jpg";
+  img9.src = "cropped_image_9.jpg";
+});
+img5.addEventListener("mouseover", function () {
+  img1.src = " cropped_0_0.jpg";
+  img2.src = "cropped_0_1.jpg ";
+  img3.src = "cropped_0_2.jpg ";
+  img4.src = "cropped_1_0.jpg ";
+  img5.src = " cropped_1_1.jpg";
+  img6.src = " cropped_1_2.jpg";
+  img7.src = " cropped_image_7.jpg";
+  img8.src = " cropped_image_8.jpg";
+  img9.src = " cropped_image_9.jpg";
+});
+img6.addEventListener("mouseover", () => {
+  img1.src = "cropped_0_0.jpg";
+  img2.src = "cropped_0_0.jpg";
+  img3.src = "cropped_0_1.jpg";
+  img4.src = "cropped_1_0.jpg";
+  img5.src = "cropped_1_0.jpg";
+  img6.src = "cropped_1_1.jpg";
+  img7.src = "cropped_image_7.jpg";
+  img8.src = "cropped_image_7.jpg";
+  img9.src = "cropped_image_8.jpg";
+});
+img7.addEventListener("mouseover", () => {
+  img1.src = "cropped_0_1.jpg";
+  img2.src = "cropped_0_2.jpg";
+  img3.src = "cropped_0_2.jpg";
+  img4.src = "cropped_0_1.jpg";
+  img5.src = "cropped_0_2.jpg";
+  img6.src = "cropped_0_2.jpg";
+  img7.src = "cropped_1_1.jpg";
+  img8.src = "cropped_1_2.jpg";
+  img9.src = "cropped_1_2.jpg";
+});
+img8.addEventListener("mouseover", () => {
+  img1.src = "cropped_0_0.jpg";
+  img2.src = "cropped_0_1.jpg";
+  img3.src = "cropped_0_2.jpg";
+  img4.src = "cropped_0_0.jpg";
+  img5.src = "cropped_0_1.jpg";
+  img6.src = "cropped_0_2.jpg";
+  img7.src = "cropped_1_0.jpg";
+  img8.src = "cropped_1_1.jpg";
+  img9.src = "cropped_1_2.jpg";
+});
+img9.addEventListener("mouseover", () => {
+  img1.src = "cropped_0_0.jpg";
+  img2.src = "cropped_0_0.jpg";
+  img3.src = "cropped_0_1.jpg";
+  img4.src = "cropped_0_0.jpg";
+  img5.src = "cropped_0_0.jpg";
+  img6.src = "cropped_0_1.jpg";
+  img7.src = "cropped_1_0.jpg";
+  img8.src = "cropped_1_0.jpg";
+  img9.src = "cropped_1_1.jpg";
+});
